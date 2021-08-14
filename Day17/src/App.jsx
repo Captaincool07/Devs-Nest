bebc7e62d8012b57aaec097d3efcfa60e67d3b63
Captaincool07 <shindeamol181195@gmail.com>
2021-08-14 18:17:07 +0530

    final Commit

--- Day17/src/App.jsx
+++ Day17/src/App.jsx
@@ -1,21 +1,28 @@
-import './component/Card';
-import './App.css';
+import React from 'react';
 import Card from './component/Card';
+import "./App.css";
 
 function App(props) {
   return (
-    <div className="App">
-     <Card title="Pizza" cal="56"/>
-     <Card title="Burger" cal="69"/>
-     <Card title="Coke" cal="500"/>
-     <Card title="Browne" cal="180"/>
-     <Card title="Fried Rice" cal="90"/>
-     <Card title="Lassania" cal="200"/>
-     <Card title="Pani Puri" cal="10"/>
-
-     
+    <div>
+     <div className="App">
+       <div className="container">
+         <div className="head">
+           <h1><strong>Calorie Dashboard</strong> </h1>
+         </div>
+         <Card title= "samosa" cal= "500" />
+         <Card title="Pizza" cal="56"/>
+        <Card title="Burger" cal="69"/>
+        <Card title="Coke" cal="500"/>
+        <Card title="Browne" cal="180"/>
+        <Card title="Fried Rice" cal="90"/>
+        <Card title="Lassania" cal="200"/>
+        <Card title="Pani Puri" cal="10"/>
+       </div>
+     </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
--- Day17/src/component/card.jsx
+++ Day17/src/component/card.jsx
@@ -1,14 +1,11 @@
 const Card = (props) =>{
- return(
-     <div className="container">
-    <div className="card">
-        
-        <h1>{props.title}</h1>
-        <h2>you have consumed {props.cal} cals today</h2>
-        
-    </div>
-    </div>
- );  
+return(
+<div className="card">
+    <div className="title"><h1>{props.title}</h1></div>
+    <div className="cal"><h2>you have consumed {props.cal} cals today</h2></div>
+</div>
+);
 };
 
 export default Card;
+
